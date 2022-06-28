d42aeb69e93e0dd16444fc8d180f05df47728c14
Naailul Ghufron <cbnd011@cabinindo.co.id>
2022-06-28 07:42:19 +0700

    trial data and filter data

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../Components/Layouts'
+import data from '../utils/data'
 
 export default function Home() {
+  const data_filter = data.items.filter((item) => item.qty >= 30)
   return (
     <Layout>
       <div>
         <h1>Menu</h1>
         <ul>
-          <li>Cash Advance</li>
+          {data_filter.map((item) => (
+            <li key={item.id}>{item.qty}</li>
+          ))}
           <li>Cash Advance</li>
           <li>Cash Advance</li>
           <li>Cash Advance</li>
---
+++ utils/data.js
@@ -0,0 +1,33 @@
+const data = {
+  items : [
+    {
+      id : 1,
+      code : "bz010",
+      name : "door handle",
+      uom : "pcs",
+      color: "white",
+      qty : 10,
+      location : "as-001",
+    },
+    {
+      id : 2,
+      code : "bz011",
+      name : "door handle",
+      uom : "pcs",
+      color: "blue",
+      qty : 20,
+      location : "as-003",
+    },
+    {
+      id : 3,
+      code : "bz013",
+      name : "door handle",
+      uom : "pcs",
+      color: "yellow",
+      qty : 30,
+      location : "as-003",
+    },
+  ]
+}
+
+export default data;
